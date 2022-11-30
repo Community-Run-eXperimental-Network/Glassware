aba1c062523dd7608b077a7c6e2a20c13f56e4a7
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2022-11-30 22:23:33 +0200

    Initial commit

---
+++ source/app.d
@@ -0,0 +1,63 @@
+import std.stdio;
+
+import vibe.d;
+import std.json;
+import std.string;
+import std.net.curl;
+
+string edbBaseURL = "https://codeberg.org/api/v1/repos/crxn/entitydb/";
+
+/** 
+ * Get the networks registered
+ *
+ * Returns: A <code>string[]</code> containing a list of all
+ * registered networks
+ */
+string[] fetchNetworks()
+{
+	string[] networks;
+
+	JSONValue jsonParsed;
+	jsonParsed = parseJSON(get(edbBaseURL~"contents"));
+
+	foreach(JSONValue networkBlock; jsonParsed.array())
+	{
+		string filename = networkBlock["name"].str();
+
+		// It's only a network if the filename ends in `.json`
+		if(endsWith(filename, ".json"))
+		{
+			writeln("Found network: "~filename);
+			networks ~= filename;
+		}
+		
+	}
+
+
+	return networks;
+}
+
+
+JSONValue fetchNetwork(string filename)
+{
+	JSONValue jsonParsed;
+
+	jsonParsed = parseJSON(get(edbBaseURL~"contents"));
+
+	return jsonParsed;
+}
+
+void main(string[] args)
+{
+	writeln("Edit source/app.d to start your project.");
+
+	// TODO: Add command-line handling here and configurtion file parsing
+
+	writeln(fetchNetwork("bruh").toPrettyString());
+
+
+	fetchNetworks();
+
+	// Start the web server
+	// runApplication();
+}
